5cf485a02b43b2f531eb992af433e72f9de57567
StevenLuu1 <steven.luu@dawsoncollege.qc.ca>
2024-10-02 15:03:37 -0400

    test 1.551 form

--- action.php
+++ action.php
@@ -9,7 +9,7 @@
             This is what you said about egg!: <?php echo htmlspecialchars($_POST['message']); ?>
             It seems you eat <?php echo (int) $_POST ['egg']; ?> a day.
             You like     <?php echo htmlspecialchars ($_POST ['favegg']); ?> egg.
-            You eat them during <?php echo ($_POST['breakfast']); ?> <?php echo ,($_POST['dinner']); ?> <?php echo ($_POST['supper']); ?> <?php echo (a); ?> <?php echo hh; ?> <?php echo "awdaw"; ?> <?php echo 'mayy'; ?>
+            You eat them during <?php echo ,($_POST['breakfast']); ?>
         </div>
 
     </body>
