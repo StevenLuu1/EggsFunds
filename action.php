3f8ea8ae6320d27143e4cfeae4b960cd16be2aec
StevenLuu1 <steven.luu@dawsoncollege.qc.ca>
2024-10-02 14:15:16 -0400

    form in form

---
+++ action.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <p>GET: <?= var_dump($_GET) ?></p>
+        <p>POST: <?= var_dump($_POST) ?></p>
+
+    </body>
+</html>
