312160debd4a5acd045b4c9ef498573057ff6b26
StevenLuu1 <steven.luu@dawsoncollege.qc.ca>
2024-10-02 14:55:50 -0400

    display of info form test

--- action.php
+++ action.php
@@ -4,8 +4,13 @@
         
     </head>
     <body>
-        <p>GET: <?= var_dump($_GET) ?></p>
-        <p>POST: <?= var_dump($_POST) ?></p>
+        <div>
+            Hello <?php echo htmlspecialchars ($_POST['fname']) ($_POST['lname']); ?>
+            This is what you said about egg!: <?php echo htmlspecialchars($_POST['message']); ?>
+            It seems you eat <?php echo (int) $_POST ['egg']; ?> a day.
+            You like to <?php echo htmlspecialchars ($_POST ['favegg']); ?> egg.
+            You eat them during <?php echo ($_POST['breakfast']), echo ($_POST['dinner']), echo ($_POST['supper']); ?>
+        </div>
 
     </body>
 </html>
